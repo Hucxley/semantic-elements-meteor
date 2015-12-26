48bb9008ff16b6afd06fc92afa4fd25582e49f5f
thoughtcrime <shane.the.timewaster@gmail.com>
2015-12-25 20:10:43 -0600

    progress bar onclick advance

--- semantic-elements-meteor.js
+++ semantic-elements-meteor.js
@@ -1,23 +1,55 @@
 if (Meteor.isClient) {
   // counter starts at 0
+  if (Session.get('counter')) {
+    var currentCount = Session.get('counter')
+    if (currentCount > 99) {
+      Session.set('counter', 0)
+    }else{
+      Session.set('counter',Session.get('counter'))
+    }
+  }
   Session.setDefault('counter', 0);
 
+  Template.hello.onRendered = function() {
+    var currentCount = Session.get('counter');
+    $('#progressBar').progress({
+      value: currentCount,
+    })
+
+  }
+
   Template.hello.helpers({
-    counter: function () {
+    counter: function() {
       return Session.get('counter');
     }
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    'click button': function() {
+      var currentCount = Session.get('counter');
+      if (currentCount <= 99) {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+        var currentCount = Session.get('counter');
+        $('#progressBar').progress({
+          value: currentCount,
+        })
+      } else {
+        Session.set('counter', 0);
+      }
     }
   });
+
+  Template.carousel.rendered = function() {
+    $('#carousel').slick({
+      dots: true,
+      arrows: true
+    });
+  }
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
